bcc172d1752e468f95e19a3072ea059d52c609e4
Antonio Expósito Solis <antonioexposito6@hotmail.com>
2017-02-23 22:13:46 +0200

    Resultados buscandos y mostrados por pantalla

--- web.py
+++ web.py
@@ -3,16 +3,21 @@
 Extra point de la assignatura de sistemes i comunicacions web
 '''
 import urllib2
-
+from bs4 import BeautifulSoup
 class Web(object):
     def get_web(self, page):
         f = urllib2.urlopen(page)
         html = f.read()
         f.close()
         return html
+    def search_text(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        elements = soup.find("div", 'dotd-title')
+        print elements.text.strip()
+
     def main(self):
         html=self.get_web("https://www.packtpub.com/packt/offers/free-learning/")
-        print html
+        self.search_text(html)
 if __name__ == '__main__':
     web = Web()
     web.main()
